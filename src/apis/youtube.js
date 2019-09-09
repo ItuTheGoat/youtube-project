f69808cbf81751f51e7eb959d8315634f143446e
Itumeleng Mochabe <ItumelengMochabe@gmail.COM>
2019-09-09 13:18:28 +0200

    Added more functionality to the site, search bar and retrieving information

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import Axios from 'axios';
+
 const KEY = "AIzaSyB97P4RWVTSYceo3K8adgboQZDm2kfWuyk";
+
+export default Axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: 
+    {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY 
+    }
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component
 {
+
+    state = { videos: [] };
+
+    // Asynchronous function to get the search term a
+    // and use it to get the data from youtube api. 
+    onTermSubmit = async term =>
+    {
+        const response = await youtube.get('/search', 
+        {
+            params: 
+            {
+                q: term
+            }
+        });
+
+        
+        this.setState({videos: response.data.items})
+    };
 
     render()
     {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}> 
-                <SearchBar />
+                <SearchBar onFormSubmit = { this.onTermSubmit }/>
+                I have { this.state.videos.length } videos.
             </div>
             );
     }
