3f1023dae610c08e45c4b8b0f8eed28cd87dff4f
Itumeleng Mochabe <ItumelengMochabe@gmail.COM>
2019-09-10 10:31:03 +0200

    added an iframe which plays the video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -7,7 +7,12 @@ const VideoDetail = ({ video }) =>
         return <div> Loading...</div>;
     }
 
+    const VideoSrc = `https://www.youtube.com/embed/${ video.id.videoId }`;
+
     return <div>
+        <div className="ui embed">
+            <iframe src= { VideoSrc } />
+            </div> 
                 <div className = "ui segment">
                     <h4 className="ui header"> { video.snippet.title } </h4>
                     <p> { video.snippet.description } </p>
